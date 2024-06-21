d7c16804cb8a8ce72be2ca3441d2eb6e854f4224
NEXRO <105696039+nex-ro@users.noreply.github.com>
2024-06-21 21:40:30 +0700

    atur login

--- View/login.php
+++ View/login.php
@@ -6,6 +6,11 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <div class="" style="display: flex;justify-content: center; align-items: center; height: 500px">
+    <div class=""style=' padding:20px;' >
+        <a href="index.php?page=dashboard"><button style="width: 100% ;padding: 10px; border-radius: 10px; margin: 5px;">Masuk</button></a>
+        <a href=""><button style="width: 100%;padding: 10px; border-radius: 10px;margin: 5px;">Login With Google</button></a>
+    </div>
+    </div>
 </body>
 </html>

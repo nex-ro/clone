1c8083fd70dcced55ef063f95684845e7d649d72
NEXRO <105696039+nex-ro@users.noreply.github.com>
2024-06-23 15:35:10 +0700

    del secret code

--- Core/configGoogle.php
+++ Core/configGoogle.php
@@ -1,7 +1,7 @@
 <?php 
 require_once 'vendor/autoload.php';
-$clientID = '212828814307-9lp9uspfhi6m1q4pglsmd7edl1713cc8.apps.googleusercontent.com';
-$clientSecret = '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';
+$clientID = '<client_id>';
+$clientSecret = '<client_secret>';
 $redirectUri = 'http://localhost/clone/index.php?page=handleLogin';
 
 $client = new Google_Client();
